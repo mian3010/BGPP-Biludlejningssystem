9e9904af85570afeabb431eff95cf568eec38c5a
Toke Jensen <tokeloke@gmail.com>
2011-12-01 14:39:49 +0100

    Toke someomfoefe

--- src/bgpp2011/Controller.java
+++ src/bgpp2011/Controller.java
@@ -8,10 +8,38 @@ public class Controller
 	private ArrayList<Vehicle> vehicles;
 	private ArrayList<Customer> customer;
 	private ArrayList<Reservation> reservations;
+	/*
+	 * Controlleren must execute operations given by the user. It must do all the checking and ordering.
+	 * 
+	 */
 	
-	
-	public void makeReservation(Vehicle v, Customer c, Date startdate, Date enddate)
+	public boolean checkReservation(Reservation re)
 	{
-		for()
-	}
+		
+		int o = re.getStartdate().compareTo(re.getEnddate());
+		if( o<0 )
+		{
+		/*
+		 * Remember to check if startdate the same or more than ennddate.
+		 */
+		for(Reservation r : reservations)
+		{
+			Vehicle vt = r.getVehicle();
+			if(vt.getId() == r.getVehicle().getId()) {
+			int startValue = re.getStartdate().compareTo(r.getEnddate());
+			int endValue = re.getEnddate().compareTo(r.getStartdate());
+			if(startValue < 0 && endValue > 0)
+			{
+				
+				return false;
+			  }
+			
+			    }			
+		    
+		           }
+			
+		             }
+		               return true;
+		             }
+	
 }
