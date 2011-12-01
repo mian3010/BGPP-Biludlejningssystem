f03b3b93c07acf12a33249492e7fda251d69d2b5
Michael Soeby Andersen <michael@mileratec.com>
2011-12-01 12:45:43 +0100

    TableModel også...

---
+++ src/bgpp2011/TableModel.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bgpp2011;
+import javax.swing.table.*;
+
+/**
+ *
+ * @author Michael
+ */
+public class TableModel extends DefaultTableModel {
+    private String[] columnNames;
+    private Object[][] data;
+
+    public TableModel(String[] columnNames, Object[][] data)
+    {
+        this.columnNames = columnNames;
+        this.data = data;
+        
+    }
+    
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return data.length;
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+        return data[row][col];
+    }
+
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+
+    /*
+     * Don't need to implement this method unless your table's
+     * editable.
+     */
+    public boolean isCellEditable(int row, int col) {
+        //Note that the data/cell address is constant,
+        //no matter where the cell appears onscreen.
+        if (col < 2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /*
+     * Don't need to implement this method unless your table's
+     * data can change.
+     */
+    public void setValueAt(Object value, int row, int col) {
+        data[row][col] = value;
+        fireTableCellUpdated(row, col);
+    }
+
+}
