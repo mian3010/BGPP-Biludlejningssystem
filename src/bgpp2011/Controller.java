eb3fbc2e43349e5f1bd2d01a78e2531581169619
Toke Jensen <tokeloke@gmail.com>
2011-12-01 15:23:52 +0100

    Controller class

--- src/bgpp2011/Controller.java
+++ src/bgpp2011/Controller.java
@@ -13,33 +13,60 @@ public class Controller
 	 * 
 	 */
 	
+	/*
+	 * The checkreservation() method checks if there is an overlap with another reservation. It takes a
+	 * reservation as a parameter. First, it checks whether the startdate is before the enddate. Second,
+	 * It checks whether the Vehicle of the Reservations re is the same as i the ArrayList. Then it checks
+	 * whether the startdate is after the enddate, or if the enddate is before the startdate. If that is true,
+	 * the reservation is possible and it will return true.
+	 */
 	public boolean checkReservation(Reservation re)
 	{
 		
 		int o = re.getStartdate().compareTo(re.getEnddate());
 		if( o<0 )
 		{
-		/*
-		 * Remember to check if startdate the same or more than ennddate.
-		 */
-		for(Reservation r : reservations)
-		{
-			Vehicle vt = r.getVehicle();
-			if(vt.getId() == r.getVehicle().getId()) {
-			int startValue = re.getStartdate().compareTo(r.getEnddate());
-			int endValue = re.getEnddate().compareTo(r.getStartdate());
-			if(startValue < 0 && endValue > 0)
+			/*
+			 * Remember to check if startdate the same or more than ennddate.
+			 */
+			for(Reservation r : reservations)
 			{
-				
+				Vehicle vt = r.getVehicle();
+				if(vt.getId() == r.getVehicle().getId()) 
+				{
+					int startValue = re.getStartdate().compareTo(r.getEnddate());
+					int endValue = re.getEnddate().compareTo(r.getStartdate());
+					if(startValue < 0 && endValue > 0)
+						{
+							return false;
+			  
+						}
+			    	
+				}	
+            }
+		           
+			         
+         } 
+		return true;
+	}	
+
+		             
+	/*
+	 * Method, that makes a new reservation and adds it to the ArrayList called reservations.
+	 * It uses the checkReservations() method to see if it is possible to make the reservations.
+	 */
+	public boolean makeReservation(int id, Customer c, Vehicle v, Date start, Date end)
+	{
+		Reservation reservation = new Reservation(id, c, v, start, end);
+			if(checkReservation(reservation))
+			{
+				reservations.add(reservation);
+				return true;
+			}
+			else {
 				return false;
-			  }
+			}
 			
-			    }			
-		    
-		           }
-			
-		             }
-		               return true;
-		             }
+	}
 	
 }
