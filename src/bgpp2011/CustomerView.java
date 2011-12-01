1a41f076047fef4abfb80105b4016dc5ad781d94
Michael Soeby Andersen <michael@mileratec.com>
2011-12-01 13:15:24 +0100

    commit

--- src/bgpp2011/CustomerView.java
+++ src/bgpp2011/CustomerView.java
@@ -2,6 +2,7 @@ package bgpp2011;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
 import java.util.*;
 
 /**
@@ -19,13 +20,13 @@ public class CustomerView extends View {
     public CustomerView(Canvas canvas)
     {
         super(canvas);
-        customers = new ArrayList();
+        customers = new ArrayList<Customer>();
         topText = "Customers";
-        customers.add(new Customer(1, "Toke Jensen", 11111111, "Vej 1, By 1"));
-        customers.add(new Customer(2, "Magnus Stahl", 22222222, "Vej 2, By 2"));
-        customers.add(new Customer(3, "Michael Søby Andersen", 33333333, "Vej 3, By 3"));
-        customers.add(new Customer(4, "Jens Jensen", 44444444, "Vej 4, By 4"));
-        customers.add(new Customer(5, "Lars Larsen", 55555555, "Vej 5, By 5"));
+        customers.add(new Customer(1, "Toke Jensen", 11111111, "Vej 1, By 1", "Konto 1"));
+        customers.add(new Customer(2, "Magnus Stahl", 22222222, "Vej 2, By 2", "Konto 2"));
+        customers.add(new Customer(3, "Michael Søby Andersen", 33333333, "Vej 3, By 3", "Konto 3"));
+        customers.add(new Customer(4, "Jens Jensen", 44444444, "Vej 4, By 4", "Konto 4"));
+        customers.add(new Customer(5, "Lars Larsen", 55555555, "Vej 5, By 5", "Konto 5"));
     }
     @Override
     public JPanel draw()
@@ -45,8 +46,8 @@ public class CustomerView extends View {
         JPanel boxLayout = new JPanel();
         boxLayout.setLayout(new BoxLayout(boxLayout, BoxLayout.Y_AXIS));
         
-        ArrayList<JButton> buttons = new ArrayList();
-        ArrayList<View> views = new ArrayList();
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
+        ArrayList<View> views = new ArrayList<View>();
         
         buttons.add(new JButton("< Back"));
         buttons.add(new JButton("Add Customer"));
