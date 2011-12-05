2a832c22643386830f443b974e9683f795105ab2
Michael Soeby Andersen <michael@mileratec.com>
2011-12-05 10:32:21 +0100

    Hent fra db

--- src/bgpp2011/CustomerView.java
+++ src/bgpp2011/CustomerView.java
@@ -2,10 +2,12 @@ package bgpp2011;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.DefaultTableModel;
 import java.lang.reflect.*;
 
 import java.util.*;
+import java.util.Map.Entry;
 
 /**
  *
@@ -17,104 +19,55 @@ import java.util.*;
  */
 public class CustomerView extends View {
     
-    private ArrayList<Customer> customers;
+    private HashMap<Integer, Customer> customers;
+    private JTable table;
     
     public CustomerView(Canvas canvas)
     {
         super(canvas);
-        customers = new ArrayList<Customer>();
         topText = "Customers";
-        customers.add(new Customer(1, "Toke Jensen", 11111111, "Vej 1, By 1", "Konto 1"));
-        customers.add(new Customer(2, "Magnus Stahl", 22222222, "Vej 2, By 2", "Konto 2"));
-        customers.add(new Customer(3, "Michael Søby Andersen", 33333333, "Vej 3, By 3", "Konto 3"));
-        customers.add(new Customer(4, "Jens Jensen", 44444444, "Vej 4, By 4", "Konto 4"));
-        customers.add(new Customer(5, "Lars Larsen", 55555555, "Vej 5, By 5", "Konto 5"));
+        Controller controller = new Controller();
+        customers = controller.getCustomers();
     }
     @Override
     public JPanel draw()
     {
         super.draw();
-        String[] columnNames = {"ID", "Name", "Address", "Phone Number", "Account", "Remove"};
+        String[] columnNames = {"ID", "Name", "Address", "Phone Number", "Account", "Show reservations", "Remove"};
         Object[][] data = parseObjects(customers);
-        int[] columnSizes = {10,1000,1000,200,200,100};
-        //TableModel model = new TableModel(columnNames, data);
+        int[] columnSizes = {10,1000,1000,200,200,100, 100};
        
-        JTable table = createTable(columnNames, data, columnSizes);
+        table = createTable(columnNames, data, columnSizes);
         JScrollPane scrollPane = new JScrollPane(table);
         
         scrollPane.setBorder(BorderFactory.createEmptyBorder(0,10,0,10));
         contentPane.add(scrollPane, BorderLayout.CENTER);
         
-        JPanel boxLayout = new JPanel();
-        boxLayout.setLayout(new BoxLayout(boxLayout, BoxLayout.Y_AXIS));
-        
-        ArrayList<JButton> buttons = new ArrayList<JButton>();
-        ArrayList<Object> views = new ArrayList<Object>();
-        ArrayList<Object> params = new ArrayList<Object>();
-        
-        buttons.add(new JButton("< Back"));
-        buttons.add(new JButton("Add Customer"));
-        
-        views.add(new FrontPageView(canvas));
-        views.add("addCustomer");
-        
-        params.add(null);
-        params.add(table);
-        
-        Iterator<JButton> i1 = buttons.iterator();
-        Iterator<Object> i2 = views.iterator();
-        Iterator<Object> i3 = params.iterator();
-        while (i1.hasNext() && i2.hasNext() && i3.hasNext())
-        {        
-            final JButton button = i1.next();
-            final Object object = i2.next();
-            final Object parameters = i3.next();
-            final View view = this;
-            
-            button.setMaximumSize(new Dimension(130,30));
-            boxLayout.add(button);
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                	try
-                	{
-                		canvas.changeView((View)object);
-                	}
-                	catch (ClassCastException e1)
-                	{
-                		try
-                		{
-                			Method m = view.getClass().getMethod(object.toString(), JTable.class);
-                			m.invoke(view, (JTable)parameters);
-                		}
-                		catch (NoSuchMethodException e2){}
-                		catch (InvocationTargetException e2){}
-                		catch (IllegalAccessException e2) {}
-                		
-                	}
-                    
-                }
-            });
-        }
+        JPanel boxLayout = createButtons();
         contentPane.add(boxLayout, BorderLayout.WEST);
         
         return contentPane;
     }
-    public Object[][] parseObjects(ArrayList<Customer> dataObjects)
+    public Object[][] parseObjects(HashMap<Integer, Customer> dataObjects)
     {
-        Object[][] data = new Object[dataObjects.size()][6];
-        for (int j = 0; j < dataObjects.size(); j++)
+        Object[][] data = new Object[dataObjects.size()][7];
+        Iterator<Entry<Integer, Customer>> i = dataObjects.entrySet().iterator();
+        int j = 0;
+        while (i.hasNext())
         {
-            data[j][0] = (int)dataObjects.get(j).getId();
-            data[j][1] = (String)dataObjects.get(j).getName();
-            data[j][2] = (int)dataObjects.get(j).getNumber();
-            data[j][3] = (String)dataObjects.get(j).getAddress();
-            data[j][4] = (String)dataObjects.get(j).getBankAccount();
+        	Map.Entry<Integer, Customer> object = (Map.Entry<Integer, Customer>)i.next();
+            data[j][0] = object.getValue().getId();
+            data[j][1] = object.getValue().getName();
+            data[j][2] = object.getValue().getNumber();
+            data[j][3] = object.getValue().getAddress();
+            data[j][4] = object.getValue().getBankAccount();
             data[j][5] = (Boolean)false;
+            data[j][6] = (Boolean)false;
+            j++;
         }
         return data;
     }
-    public void addCustomer(JTable table)
+    public void addEntry(JTable table)
     {
         addToTable(table);
     }
@@ -127,5 +80,21 @@ public class CustomerView extends View {
     	JTable table = super.createTable(columnNames, data, columnSizes);
     	
     	return table;
+    }
+    public void tableChanged(TableModelEvent e)
+    {
+    	super.tableChanged(e);
+    	int row = e.getFirstRow();
+    	int column = e.getColumn();
+    	
+    	switch (column)
+    	{
+    	case 5:
+    		canvas.changeView(new ReservationView(canvas, (int)table.getValueAt(row, 0)));
+    	break;
+    	default:
+    		
+    	break;
+    	}
     }
 }
--- src/bgpp2011/ReservationView.java
+++ src/bgpp2011/ReservationView.java
@@ -2,6 +2,7 @@ package bgpp2011;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
 import java.util.*;
 
 /**
@@ -9,9 +10,26 @@ import java.util.*;
  * @author Michael
  */
 public class ReservationView extends View {
+	
+	private ArrayList<Reservation> reservations;
+    private JTable table;
+    
     public ReservationView(Canvas canvas)
     {
         super(canvas);
+        reservations = new ArrayList<Reservation>();
+        topText = "Reservations";
+/*        reservations.add(new Reservation(1, new Customer(1, "Toke Jensen", 11111111, "Vej 1, By 1", "Konto 1"), ));
+        reservations.add(new Reservation(2, new Customer(1, "Toke Jensen", 11111111, "Vej 1, By 1", "Konto 1"),));
+        reservations.add(new Reservation(3, new Customer(2, "Magnus Stahl", 22222222, "Vej 2, By 2", "Konto 2"),));
+        reservations.add(new Reservation(4, new Customer(3, "Michael Søby Andersen", 33333333, "Vej 3, By 3", "Konto 3"),));
+        reservations.add(new Reservation(5, new Customer(4, "Jens Jensen", 44444444, "Vej 4, By 4", "Konto 4"),));
+        */
+        
+    }
+    public ReservationView(Canvas canvas, int customerID)
+    {
+        this(canvas);
         
     }
     @Override
